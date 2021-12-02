fbdce77bb1073a102e02e3b03d6e1a831c4182cc
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2021-12-02 20:00:04 +0200

    search filters

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+import { filterData, getFilterValues } from "../utils/filterData";
+
+const SearchFilters = () => {
+  const [filters, setFilters] = useState(filterData);
+  const router = useRouter();
+
+  const searchProperties = (filterValues) => {
+    const { pathname, query } = router;
+    const values = getFilterValues(filterValues);
+
+    values.forEach((value) => (query[value.name] = value.value));
+    router.push({ pathname, query });
+  };
+
+  return (
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+          >
+            {filter?.items?.map((item) => (
+              <option key={item.value} value={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+      <Input
+        placeholder="Search By Location"
+        onChange={(e) => searchProperties({ search: e.target.value })}
+      />
+    </Flex>
+  );
+};
+
+export default SearchFilters;
